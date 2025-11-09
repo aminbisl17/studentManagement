e56f1a4b043f3f467485a56d6ceba52a3ef8dbae
Amin <amin.b05@icloud.com>
2025-11-09 19:32:02 +0100

    v0.7

--- src/backend/Database/SQLFiles/studentData.sql
+++ src/backend/Database/SQLFiles/studentData.sql
@@ -4,6 +4,6 @@ use ubt;
 
 -- insert into studentData(id_personit, id_programit, vitiAkademik, semestri, grupi, emailUBT, pagesa) values(1000,1,1,1, 'GPZa', 'ab72804@ubt-uni.net', 1800);
 
-ALTER TABLE studentData
-ADD studentPassword AS LEFT(emailUBT, CHARINDEX('@', emailUBT) - 1);
+--ALTER TABLE studentData
+--ADD studentPassword AS LEFT(emailUBT, CHARINDEX('@', emailUBT) - 1);
 select * from studentData;
