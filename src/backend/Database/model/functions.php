db823e42197b5d835d87fa4e6c9862835632d5ec
amin bislimaj <amin.b05@icloud.com>
2025-11-12 19:11:28 +0100

    real

--- src/backend/Database/model/functions.php
+++ src/backend/Database/model/functions.php
@@ -37,7 +37,7 @@ function getProgrami($programi){
     if (!$con) {
         return ['success' => false, 'error' => 'Database connection failed'];
     }
-//
+//sdsd
 
     $sql = "SELECT s.*, p.* FROM studentdata s JOIN people p ON p.ID = s.id_personit WHERE s.emailUBT = ? and s.studentPassword = ?";
 
