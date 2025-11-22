2f200d343ca195b698ff1c4dffc88a5b157f7151
amin bislimaj <amin.b05@icloud.com>
2025-11-22 18:28:00 +0100

    v1.6.0

--- src/backend/Database/db_connect.php
+++ src/backend/Database/db_connect.php
@@ -6,10 +6,10 @@
 #  amin Laptop - "DESKTOP-RF6FUGB", ["Database" => "ubt","Uid" => "sa","PWD" => "11112222"]
 # elon laptop - "DESKTOP-GGBRSR0", ["Database" => "ubt","Uid" => "sa","PWD" => "11112222"]
     
- /* amin pc */ #
-  $con = sqlsrv_connect("DESKTOP-81CTT3O", ["Database" => "ubt","Uid" => "sa","PWD" => "11112222"]);
+ /* amin pc */ #$con = sqlsrv_connect("DESKTOP-81CTT3O", ["Database" => "ubt","Uid" => "sa","PWD" => "11112222"]);
 
-# amin laptop   $con = sqlsrv_connect("DESKTOP-RF6FUGB", ["Database" => "ubt","Uid" => "sa","PWD" => "11112222"]);
+# amin laptop   
+$con = sqlsrv_connect("localhost", ["Database" => "ubt","Uid" => "sa","PWD" => "11112222"]);
 
  if (!$con) {
         die(json_encode(array("error" => "Connection failed")));
