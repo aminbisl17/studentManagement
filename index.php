fdbf89d81a7a61a0dd46f1c7d530d44c961920ca
Amin <amin.b05@icloud.com>
2025-11-23 02:36:25 +0100

    v1.10.0

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
-header("Location: /studentManagement/src/frontend/static/main.html");
+// header("Location: /studentManagement/src/frontend/static/main.html");
+
+header("Location: src/frontend/static/main.html");
 exit();
 ?>
